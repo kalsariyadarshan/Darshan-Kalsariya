f3e5f5d336b6779c7c6fbb7c2c68bf3c6ef59bce
kalsariyadarshan <142231808+kalsariyadarshan@users.noreply.github.com>
2024-10-22 21:41:27 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  experimental: {
+    swcLoader: true, // Enable SWC loader (optional)
+    swcMinify: true, // Enable SWC minifier
+    forceSwcTransforms: true, // Fallback to WASM
+  },
+};
+
+module.exports = nextConfig;
